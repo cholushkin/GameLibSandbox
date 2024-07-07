071287aa8ecbcdeb6ef47f9dceed16364fea72da
Max <cholushkin@gmail.com>
2024-07-07 23:02:17 +0300

    remove: unittest (use std test runner instead)

--- GameLibSandboxUnity/Assets/Sandbox/CaseManager/CasesManager.cs
+++ GameLibSandboxUnity/Assets/Sandbox/CaseManager/CasesManager.cs
@@ -5,34 +5,34 @@ using UnityEngine.Assertions;
 
 public class CasesManager : MonoBehaviour
 {
-    public SceneDependenciesConfig SceneDependenciesConfig;
-    public SceneLoader SceneLoader;
-    public LogChecker Log;
-    private string _currentSceneName;
-    private string _previousSceneName;
-
-	void Start()
-    {
-        RunCase(GetNextSceneName());
-    }
-
-    public void RunCase(string sceneName)
-    {
-	    _previousSceneName = _currentSceneName;
-		SceneLoader.LoadSequence(sceneName);
-    }
-
-    public string GetNextSceneName()
-    {
-		// If _currentSceneName is not initialized yet then return the first scene from SceneDependenciesConfig
-		if (string.IsNullOrEmpty(_currentSceneName))
-		    return SceneDependenciesConfig.AllSceneDependencies[0].DevSceneOrWildcard;
-
-	    int index = SceneDependenciesConfig.AllSceneDependencies.Select((p, idx) => new { p, idx })
-		    .FirstOrDefault(x => x.p.DevSceneOrWildcard == _currentSceneName)?.idx ?? -1;
-        Assert.IsTrue(index != -1);
-
-		var nextIndex = (index + 1) % SceneDependenciesConfig.AllSceneDependencies.Length;
-		return SceneDependenciesConfig.AllSceneDependencies[nextIndex].DevSceneOrWildcard;
-    }
+ //    public SceneDependenciesConfig SceneDependenciesConfig;
+ //    public SceneLoader SceneLoader;
+ //    public LogChecker Log;
+ //    private string _currentSceneName;
+ //    private string _previousSceneName;
+ //
+	// void Start()
+ //    {
+ //        RunCase(GetNextSceneName());
+ //    }
+ //
+ //    public void RunCase(string sceneName)
+ //    {
+	//     _previousSceneName = _currentSceneName;
+	// 	SceneLoader.LoadSequence(sceneName);
+ //    }
+ //
+ //    public string GetNextSceneName()
+ //    {
+	// 	// If _currentSceneName is not initialized yet then return the first scene from SceneDependenciesConfig
+	// 	if (string.IsNullOrEmpty(_currentSceneName))
+	// 	    return SceneDependenciesConfig.AllSceneDependencies[0].DevSceneOrWildcard;
+ //
+	//     int index = SceneDependenciesConfig.AllSceneDependencies.Select((p, idx) => new { p, idx })
+	// 	    .FirstOrDefault(x => x.p.DevSceneOrWildcard == _currentSceneName)?.idx ?? -1;
+ //        Assert.IsTrue(index != -1);
+ //
+	// 	var nextIndex = (index + 1) % SceneDependenciesConfig.AllSceneDependencies.Length;
+	// 	return SceneDependenciesConfig.AllSceneDependencies[nextIndex].DevSceneOrWildcard;
+ //    }
 }
